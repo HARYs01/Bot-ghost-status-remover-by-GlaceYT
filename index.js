96a978054639e833f80607f61b4447978da3aa52
- HARYs - <yousuf.ibnesalah@gmail.com>
2024-09-11 22:50:36 +0500

    index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to Onslaught", "🎮 Playing ⚔️Hero Wars: Dominon Era⚔️"];
+const statusMessages = ["🎧 Listening to Onslaught", "🎮 Playing ⚔️Hero Wars: Dominon Era⚔️", "❓ Prefix is ?, Use ?help"];
 const statusTypes = [ 'dnd'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
